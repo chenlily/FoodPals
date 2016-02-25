c6926323c074fce9ecb1d2138a6e017bb7eea9b5
Lily Chen <chenlily@umich.edu>
2016-02-25 16:32:53 -0500

    added uiswitch and button

--- FoodPals/ViewController.swift
+++ FoodPals/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: Properties
+    @IBOutlet weak var fromText: UILabel!
+    @IBOutlet weak var availSwitch: UISwitch!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("view loaded")
+        
+        availSwitch.addTarget(self, action: Selector("availStateChanged:"), forControlEvents: UIControlEvents.ValueChanged)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: Actions
+    @IBAction func findPals(sender: UIButton) {
+        print("hello")
+    }
+    @IBAction func availSwitchPressedAction(sender: UISwitch) {
+    }
+    
+    func availStateChanged(switchState: UISwitch) {
+        if switchState.on {
+            print("its on")
+        } else {
+            print("its off")
+        }
+    }
 
 }
 
