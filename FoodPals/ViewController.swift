128f627af73c6fe2cb66ec3a6d9a80934dc2c8fa
Wei, Yunhan <yunhanw@aha33.local>
2016-03-07 23:36:49 -0500

    Added back segue for people list and a log out function

--- FoodPals/ViewController.swift
+++ FoodPals/ViewController.swift
@@ -108,6 +108,19 @@ class ViewController: UIViewController {
         // K: Update to time for current user
         user.updateValue(strTime, forKey: "to")
     }
+    @IBAction func logout(sender: AnyObject) {
+        
+        // unauth() is the logout method for the current user.
+        DataService.dataService.CURRENT_USER_REF.unauth()
+        
+        // Remove the user's uid from storage.
+        NSUserDefaults.standardUserDefaults().setValue(nil, forKey: "uid")
+        
+        // Head back to login
+        let loginViewController = self.storyboard!.instantiateViewControllerWithIdentifier("Login")
+        
+        UIApplication.sharedApplication().keyWindow?.rootViewController = loginViewController
+    }
 
     @IBAction func fromDatePickerAction(sender: AnyObject) {
         
