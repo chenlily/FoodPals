a0cf2dadd68fff58b3a4656716a93b1b78c2aed4
Wei, Yunhan <yunhanw@aha33.ccs.itd.umich.edu>
2016-03-07 19:56:20 -0500

    test commit

---
+++ FoodPals/WelcomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  WelcomeViewController.swift
+//  FoodPals
+//
+//  Created by Wei, Yunhan on 3/7/16.
+//  Copyright © 2016 PuppyCuddlers. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class WelcomeViewControlleer: UIViewController{
+    override func viewDidLoad() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
+    
+}
